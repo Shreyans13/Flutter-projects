1f358318e31b8932849f209a7b5c8609228f25f6
Shreyans_13 <shreyans.jain@juspay.in>
2021-07-01 08:48:54 +0530

    mi_card_flutter

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+Color primary = Color(0xff845ec2);
+Color secondary = Color(0xff2d8ba8);
+Color text = Color(0xffdbf2fb);
+
+class MyApp extends StatelessWidget {
+  // const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/50544190?v=4'),
+            ),
+            Text(
+              "Shreyans Jain",
+              style: GoogleFonts.pacifico(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Full Stack Developer".toUpperCase(),
+              style: GoogleFonts.playfairDisplay(
+                fontSize: 20.0,
+                color: text,
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade50,
+                )),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mobile_friendly,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '7004407831',
+                  style: GoogleFonts.sourceSansPro(
+                      color: Colors.teal.shade900, fontSize: 15),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email_outlined,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'shreyans1313@gmail.com',
+                  style: GoogleFonts.sourceSansPro(
+                    color: Colors.teal.shade900,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
